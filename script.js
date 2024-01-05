0217a149f6e0eaa3145af5198db0824bbf6c11e6
IhorSliepchenko1 <igorslepchenko.cc@gmail.comD>
2024-01-05 19:45:51 +0200

    Correction

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 let renderVS = document.getElementById('renderVS')
+const newGame = document.getElementById('newGame')
+
+const funNewGame = (() => newGame === document.location.reload())
+
 function arrayRandElement(arr) {
     let rand = Math.floor(Math.random() * arr.length);
     return arr[rand];
